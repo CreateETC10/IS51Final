841b2c0a9ee74c04aa6ce31726551103d381f3c3
CreateETC10 <greene_10@mail.fresnostate.edu>
2022-05-19 17:09:54 -0700

    finished structured-english

---
+++ Final.py
@@ -0,0 +1,9 @@
+"""
+Structured-english
+The program will analyse the grades on a final exam
+The program will point to file (Final.txt) and create a list from the text and then close the file
+We will write 3 functions and print the output to the user
+The first will output the number of grades, 
+The second will output the average grade
+The third will ouput the precentage of grades above average
+"""
